5de74a3051e99c04ca6564ea45a5fca0e4fa0298
F8Y <batigoal277@gmail.com>
2026-04-14 20:59:17 +1000

    fix: entrypoint fix

--- public/index.php
+++ public/index.php
@@ -1,39 +1,20 @@
 <?php
 
-declare(stirct_types=1);
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use DI\Container;
-use Slim\Factory\AppFactory;
-use Slim\Psr7\Response;
+$builder = new ContainerBuilder();
+$builder->addDefinitions(__DIR__ . '/../config/container.php');
 
-$container = new Container();
+$container = $builder->build();
+
 AppFactory::setContainer($container);
-
 $app = AppFactory::create();
 
-$app->get('/health', function ($request, Response $response) {
-    $response->getBody()->write(json_encode([
-        'status' => 'ok',
-        'service' => 'anonymizer-api',
-    ], JSON_UNESCAPED_UNICODE));
-
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-$app->post('/v1/anonymize', function ($request, Response $response) {
-    $body = (string) $request->getBody();
-    $payload = json_decode($body, true);
-
-    $result = [
-        'received' => $payload,
-        'message' => 'TODO: Anonymization logic will be added later'
-    ];
-
-    $response->getBody()->write(json_encode($result, JSON_UNESCAPED_UNICODE));
-
-    return $response->withHeader('Content-Type', 'application/json');
-});
+(require __DIR__ . '/../config/routes.php')($app);
 
 $app->run();
